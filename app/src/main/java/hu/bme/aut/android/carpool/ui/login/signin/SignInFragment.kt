7d4546e5f969e134e8b06cd36b20079727136989
Szabó Liza <Liza.Szabo0715@yahoo.ro>
2022-09-24 00:24:33 +0300

    Add login screen ui

--- app/src/main/java/hu/bme/aut/android/carpool/ui/login/signin/SignInFragment.kt
+++ app/src/main/java/hu/bme/aut/android/carpool/ui/login/signin/SignInFragment.kt
@@ -1,21 +1,39 @@
 package hu.bme.aut.android.carpool.ui.login.signin
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import co.zsmb.rainbowcake.base.RainbowCakeFragment
 import co.zsmb.rainbowcake.hilt.getViewModelFromFactory
 import dagger.hilt.android.AndroidEntryPoint
 import hu.bme.aut.android.carpool.R
+import hu.bme.aut.android.carpool.databinding.FragmentSigninBinding
 
 @AndroidEntryPoint
 class SignInFragment : RainbowCakeFragment<SignInViewState, SignInViewModel>() {
+
+    private lateinit var binding: FragmentSigninBinding
     override fun provideViewModel() = getViewModelFromFactory()
     override fun getViewResource() = R.layout.fragment_signin
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSigninBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // TODO Setup views
+        setupRegisterLink()
+        setupLoginButton()
     }
 
     override fun onStart() {
@@ -26,5 +44,26 @@ class SignInFragment : RainbowCakeFragment<SignInViewState, SignInViewModel>() {
 
     override fun render(viewState: SignInViewState) {
         // TODO Render state
+    }
+
+    private fun setupRegisterLink() {
+        val mSpannableString = SpannableString(binding.registrationLink.text)
+        mSpannableString.setSpan(UnderlineSpan(), 0, mSpannableString.length, 0)
+        binding.registrationLink.text = mSpannableString
+
+        binding.registrationLink.setOnClickListener {
+            Toast.makeText(context, "Go to registration", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun setupLoginButton() {
+        binding.loginButton.setOnClickListener {
+            if (binding.userNameInput.text.isEmpty()) {
+                binding.userNameInput.error = "User name cannot be empty!"
+            }
+            if (binding.passwordInput.text.isEmpty()) {
+                binding.passwordInput.error = "Password cannot be empty!"
+            }
+        }
     }
 }
