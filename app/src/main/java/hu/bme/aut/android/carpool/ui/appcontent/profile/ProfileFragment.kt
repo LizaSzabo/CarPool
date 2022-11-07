1c327e81197eca5fbd629635b15b19e9cd9fd844
Szabó Liza <Liza.Szabo0715@yahoo.ro>
2022-11-07 16:29:21 +0100

    Add gallery open when permission is granted

--- app/src/main/java/hu/bme/aut/android/carpool/ui/appcontent/profile/ProfileFragment.kt
+++ app/src/main/java/hu/bme/aut/android/carpool/ui/appcontent/profile/ProfileFragment.kt
@@ -1,14 +1,18 @@
 package hu.bme.aut.android.carpool.ui.appcontent.profile
 
 import android.Manifest.permission.READ_MEDIA_IMAGES
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import co.zsmb.rainbowcake.base.RainbowCakeFragment
 import co.zsmb.rainbowcake.hilt.getViewModelFromFactory
@@ -24,21 +28,33 @@ class ProfileFragment : RainbowCakeFragment<ProfileViewState, ProfileViewModel>(
     override fun getViewResource() = R.layout.fragment_profile
 
 
-    val requestPermissionLauncher =
+    private val requestPermissionLauncher =
         registerForActivityResult(
             ActivityResultContracts.RequestPermission()
         ) { isGranted: Boolean ->
             if (isGranted) {
                 // Permission is granted. Continue the action or workflow in your
                 // app.
+                openSomeActivityForResult()
             } else {
                 // Explain to the user that the feature is unavailable because the
                 // feature requires a permission that the user has denied. At the
                 // same time, respect the user's decision. Don't link to system
                 // settings in an effort to convince the user to change their
                 // decision.
+
             }
         }
+
+    private var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            val selectedImageUri: Uri? = data?.data
+            if (null != selectedImageUri) {
+                val yourBitmap: Bitmap = MediaStore.Images.Media.getBitmap(context?.contentResolver, selectedImageUri)
+            }
+        }
+    }
 
 
     override fun render(viewState: ProfileViewState) {
@@ -91,15 +107,16 @@ class ProfileFragment : RainbowCakeFragment<ProfileViewState, ProfileViewModel>(
                     READ_MEDIA_IMAGES
                 ) == PackageManager.PERMISSION_GRANTED -> {
                     // You can use the API that requires the permission.
+                    openSomeActivityForResult()
                 }
-               // shouldShowRequestPermissionRationale() -> {
-                    // In an educational UI, explain to the user why your app requires this
-                    // permission for a specific feature to behave as expected, and what
-                    // features are disabled if it's declined. In this UI, include a
-                    // "cancel" or "no thanks" button that lets the user continue
-                    // using your app without granting the permission.
+                // shouldShowRequestPermissionRationale() -> {
+                // In an educational UI, explain to the user why your app requires this
+                // permission for a specific feature to behave as expected, and what
+                // features are disabled if it's declined. In this UI, include a
+                // "cancel" or "no thanks" button that lets the user continue
+                // using your app without granting the permission.
 
-               // }
+                // }
                 else -> {
                     // You can directly ask for the permission.
                     // The registered ActivityResultCallback gets the result of this request.
@@ -110,6 +127,13 @@ class ProfileFragment : RainbowCakeFragment<ProfileViewState, ProfileViewModel>(
             }
         }
 
+    }
+
+    private fun openSomeActivityForResult() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        resultLauncher.launch(intent)
     }
 
 
