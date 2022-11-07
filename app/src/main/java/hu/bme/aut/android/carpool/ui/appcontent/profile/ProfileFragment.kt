6a72593ac713ff89bc80b5261aa7f0b17ba8709f
Szabó Liza <Liza.Szabo0715@yahoo.ro>
2022-11-07 11:06:45 +0100

    Add request permission for accessing gallery

--- app/src/main/java/hu/bme/aut/android/carpool/ui/appcontent/profile/ProfileFragment.kt
+++ app/src/main/java/hu/bme/aut/android/carpool/ui/appcontent/profile/ProfileFragment.kt
@@ -1,9 +1,15 @@
 package hu.bme.aut.android.carpool.ui.appcontent.profile
 
+import android.Manifest.permission.READ_MEDIA_IMAGES
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import co.zsmb.rainbowcake.base.RainbowCakeFragment
 import co.zsmb.rainbowcake.hilt.getViewModelFromFactory
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,25 +23,40 @@ class ProfileFragment : RainbowCakeFragment<ProfileViewState, ProfileViewModel>(
     override fun provideViewModel() = getViewModelFromFactory()
     override fun getViewResource() = R.layout.fragment_profile
 
+
+    val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                // Permission is granted. Continue the action or workflow in your
+                // app.
+            } else {
+                // Explain to the user that the feature is unavailable because the
+                // feature requires a permission that the user has denied. At the
+                // same time, respect the user's decision. Don't link to system
+                // settings in an effort to convince the user to change their
+                // decision.
+            }
+        }
+
+
     override fun render(viewState: ProfileViewState) {
         when (viewState) {
             is InitialProfileState -> {
                 binding.userNameEditText.visibility = View.INVISIBLE
                 binding.userNameText.visibility = View.VISIBLE
                 binding.profileImage.isEnabled = false
-                binding.addGroupMemberButton.visibility = View.INVISIBLE
             }
             is ProfileEditingState -> {
                 binding.userNameEditText.visibility = View.VISIBLE
                 binding.userNameText.visibility = View.INVISIBLE
                 binding.profileImage.isEnabled = true
-                binding.addGroupMemberButton.visibility = View.VISIBLE
             }
             is ProfileSuccessfullyEditedState -> {
                 binding.userNameEditText.visibility = View.INVISIBLE
                 binding.userNameText.visibility = View.VISIBLE
                 binding.profileImage.isEnabled = false
-                binding.addGroupMemberButton.visibility = View.INVISIBLE
             }
         }
     }
@@ -50,6 +71,7 @@ class ProfileFragment : RainbowCakeFragment<ProfileViewState, ProfileViewModel>(
         return binding.root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -62,7 +84,34 @@ class ProfileFragment : RainbowCakeFragment<ProfileViewState, ProfileViewModel>(
 
         }
 
+        binding.profileImage.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    requireActivity(),
+                    READ_MEDIA_IMAGES
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    // You can use the API that requires the permission.
+                }
+               // shouldShowRequestPermissionRationale() -> {
+                    // In an educational UI, explain to the user why your app requires this
+                    // permission for a specific feature to behave as expected, and what
+                    // features are disabled if it's declined. In this UI, include a
+                    // "cancel" or "no thanks" button that lets the user continue
+                    // using your app without granting the permission.
+
+               // }
+                else -> {
+                    // You can directly ask for the permission.
+                    // The registered ActivityResultCallback gets the result of this request.
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        requestPermissionLauncher.launch(READ_MEDIA_IMAGES)
+                    }
+                }
+            }
+        }
+
     }
+
 
 
 }
