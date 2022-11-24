0cec5e73061728b14395b68b985f4e3623b07450
Szabó Liza <Liza.Szabo0715@yahoo.ro>
2022-11-24 07:12:25 +0100

    splash screen working on every version

--- app/src/main/java/hu/bme/aut/android/carpool/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/android/carpool/MainActivity.kt
@@ -13,12 +13,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        if (Build.VERSION.SDK_INT < 31)
-            setTheme(R.style.Theme_CarPool)
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_CarPool)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         CarPoolApplication.firebaseAuth = FirebaseAuthentication()
     }
 }
